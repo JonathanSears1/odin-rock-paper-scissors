b2eaa6ad8b6052fbe9b2e2fbfe554903cb1ce8df
Jonathan Sears <jonathansears7@gmail.com>
2023-12-28 18:11:38 -0500

    finished basic ui

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ const paper = document.querySelector("#paper")
 const scissors = document.querySelector("#scissors")
 const shoot = document.querySelector("#shoot")
 const result = document.querySelector("#result")
+const userWinCount = document.querySelector("#user_wins")
+const cpuWinCount = document.querySelector("#cpu_wins")
+const container = document.querySelector(".container")
 let choiceImg = document.querySelector("#user_img_choice")
 let cpuImg = document.querySelector("#CPU_img_choice")
 let cpuText = document.querySelector("#CPU_text_choice")
 let playerChoice = ""
 
+let gamesPlayed = 0
+let userWins = 0
+let cpuWins = 0
 
 rock.addEventListener("click", () => {
     playerChoice = "rock"
@@ -49,67 +55,45 @@ function playRound(playerSelection, compterChoice = getComputerChoice()) {
     else {
     if (lowerPlayerSelection == "scissors") {
         if (compterChoice == "rock") {
+                cpuWins += 1
                 outcome = "lose"
             }
             else {
+                userWins += 1
                 outcome = "win"
             }
         }
     else if (lowerPlayerSelection == "rock") {
         if (compterChoice == "paper") {
+            cpuWins += 1
             outcome = "lose"
         }
         else {
+            userWins += 1
             outcome = "win"
         }
     }
     else if (lowerPlayerSelection == "paper") {
         if (compterChoice == "scissors") {
+            cpuWins += 1
             outcome = "lose"
         }
         else {
+            userWins += 1
             outcome = "win"
         }
     }
     else {
         return "Please select rock, paper, or scissors before clicking shoot"
     }
-}    
+}
+    gamesPlayed += 1    
     return outcome
 }
 
-function game() {
-    let playerScore = 0
-    let computerScore = 0
-    let outcome
-    let message
-    for (let i = 1;i<=5;i++) {
-        console.log(`round ${i}`)
-        message  = playRound()
-        messageArr = message.split(' ')
-        if (messageArr[messageArr.length -1] == "win!") {
-            playerScore++
-        }
-        else if (messageArr[messageArr.length -1] == "lose!") {
-            computerScore++
-        }
-        console.log(message)
-        console.log(`Player score: ${playerScore}`)
-        console.log(`Computer score: ${computerScore}`)
-    }
-    if (playerScore > computerScore) {
-        outcome = "win"
-    } 
-    else if (playerScore < computerScore) {
-        outcome = "lose"
-    }
-    else {
-        outcome = "tie"
-    }
-    return `you scored ${playerScore} and the computer scored ${computerScore}, you ${outcome}`
-}
-
 shoot.addEventListener("click", () => {
+    if (userWins < 5 && cpuWins < 5) {
+        shoot.textContent = "SHOOT!"
     let computerChoice = getComputerChoice()
     if (computerChoice == "rock") {
         cpuImg.src = "images/pngimg.com - stone_PNG13545.png"
@@ -121,5 +105,22 @@ shoot.addEventListener("click", () => {
         cpuImg.src = "images/White-Paper-No-Background.png"
     }
     cpuText.textContent = computerChoice
-    result.textContent = playRound(playerChoice,computerChoice)
+    playRound(playerChoice,computerChoice)
+    if (userWins == 5) {
+        result.textContent = "CONGRATULATIONS! YOU WIN!!!!"
+        shoot.textContent = "Play Again?"
+    }
+    if (cpuWins == 5) {
+        result.textContent = "Sorry, you lose :("
+        shoot.textContent = "Play Again?"
+    }
+    }
+    else {
+        userWins = 0
+        cpuWins = 0
+        cpuImg.src = "images/Question_mark_(black).svg.png"
+        choiceImg.src = "images/Question_mark_(black).svg.png"
+    }
+    userWinCount.textContent = `User Wins: ${userWins}`
+    cpuWinCount.textContent = `CPU Wins: ${cpuWins}`
 })
